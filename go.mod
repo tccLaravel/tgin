65c254d6bb7381f4c0b4d51b68a8208ae8182ed3
tccLaravel <527293671@qq.com>
2019-12-03 22:25:49 +0800

    log and route

--- go.mod
+++ go.mod
@@ -3,14 +3,13 @@ module tgin
 go 1.13
 
 require (
-	cloud.google.com/go v0.37.4
 	github.com/astaxie/beego v1.12.0
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.5.0
 	github.com/go-ini/ini v1.51.0
 	github.com/jinzhu/gorm v1.9.11
 	github.com/kr/pretty v0.1.0 // indirect
 	github.com/smartystreets/goconvey v1.6.4 // indirect
 	github.com/unknwon/com v1.0.1
-	gopkg.in/go-playground/validator.v9 v9.29.1
 	gopkg.in/ini.v1 v1.51.0 // indirect
 )
--- routers/route.go
+++ routers/route.go
@@ -1,24 +1,39 @@
 package routers
+
 import (
 	"github.com/gin-gonic/gin"
+	"tgin/controllers/api"
+	"tgin/middleware"
 	"tgin/pkg/setting"
-	"tgin/routers/api/v1"
 )
 func InitRouter() *gin.Engine {
 	r := gin.New()
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
 	gin.SetMode(setting.RunMode)
-	apiv1 := r.Group("/api/v1")
+	r.GET("/auth", api.GetAuth)
+	apiv1 := r.Group("/api/v1").Use(middleware.JWT())
 	{
 		//获取标签列表
-		apiv1.GET("/tags", v1.GetTags)
+		apiv1.GET("/tags", api.GetTags)
 		//新建标签
-		apiv1.POST("/tags", v1.AddTag)
+		apiv1.POST("/tags", api.AddTag)
 		//更新指定标签
-		apiv1.PUT("/tags/:id", v1.EditTag)
+		apiv1.PUT("/tags/:id", api.EditTag)
 		//删除指定标签
-		apiv1.DELETE("/tags/:id", v1.DeleteTag)
+		apiv1.DELETE("/tags/:id", api.DeleteTag)
+
+		//获取文章列表
+		apiv1.GET("/articles", api.GetArticles)
+		//获取指定文章
+		apiv1.GET("/articles/:id", api.GetArticle)
+		//新建文章
+		apiv1.POST("/articles", api.AddArticle)
+		//更新指定文章
+		apiv1.PUT("/articles/:id", api.EditArticle)
+		//删除指定文章
+		apiv1.DELETE("/articles/:id", api.DeleteArticle)
 	}
+
 	return r
 }
